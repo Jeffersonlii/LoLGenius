2e6f3105751617483a5d2de090a2bf3af50761f8
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2021-06-20 19:58:13 -0400

    secrets

--- ml/secrets.py
+++ ml/secrets.py
@@ -1,5 +1,9 @@
+import os
+
+key = os.environ.get('RIOT_API_KEY', 'add_key_here')
+
 # this key is low usage, mostly for retrieving the users current game
-API_KEY = 'RGAPI-ec4e5b71-a4b5-4c03-8d92-59c9b58b4368'
+API_KEY = key
 
 # this key is very high usage, used to gather data for machine learning
-DATA_GATHERING_API_KEY = 'RGAPI-ec4e5b71-a4b5-4c03-8d92-59c9b58b4368'
+DATA_GATHERING_API_KEY = key
